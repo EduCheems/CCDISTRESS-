27823c132ae3c7a2c6c1585d89457f8c73d51a9b
T1 Bonker <airon_bantillo@dlsu.edu.ph>
2025-03-31 19:55:50 +0800

    Create CCDSTRU.c

---
+++ CCDSTRU.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define GRIDSIZE 4
+#define UNO "('o')"
+#define TRES "(-.-)"
+#define EMPTY "     "
+
+void initializeGrid(char grid[GRIDSIZE][GRIDSIZE][6]) {
+	
+	int i, j; 
+	
+    for (i = 0; i < GRIDSIZE; i++) {
+        for (j = 0; j < GRIDSIZE; j++) {
+        	strcpy(grid[i][j], EMPTY); 
+        }
+    }
+}
+
+void printGrid(char grid[GRIDSIZE][GRIDSIZE][6]) {
+	
+	int i, j; 
+	
+    for (i = 0; i < GRIDSIZE; i++) {
+        printf("+ --- + --- + --- + --- +\n");
+        for (j = 0; j < GRIDSIZE; j++) {
+            printf("|%s", grid[i][j]);
+        }
+        printf("|\n");
+    }
+    printf("+ --- + --- + --- + --- +\n");
+}
